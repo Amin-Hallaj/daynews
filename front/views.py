076ce2e98056499e056ba35247f821b6beb90113
Amin-Hallaj <aminhallajm@gmail.com>
2023-09-01 18:54:13 +0330

    ساخت ویو درباره ما در آپ فرانت

--- front/views.py
+++ front/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render , redirect , get_object_or_404
 
 def front_index(request):
     return render(request,'front/index.html',{})
+
+
+def front_about(request):
+    return render(request,'front/about-us.html',{})
