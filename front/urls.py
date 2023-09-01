74eaa401025410fb97e76516ce138b7a88239792
Amin-Hallaj <aminhallajm@gmail.com>
2023-09-01 22:01:36 +0330

    ساخت یو ار ال برای تماس با ما در آپ فرانت

--- front/urls.py
+++ front/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.front_index,name='front_index'),
     path('about/us/',views.front_about,name='front_about'),
+    path('contact/us/',views.front_contact,name='front_contact'),
 ]
