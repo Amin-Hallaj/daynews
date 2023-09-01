7c2889f734d9a8b208cf9fd1afedd1abfa384d8b
Amin-Hallaj <aminhallajm@gmail.com>
2023-09-01 15:05:16 +0330

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path , include , re_path
+from django.conf import settings
+from django.views.static import serve
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",include('main.urls')),
+    path("",include('master.urls')),
+    path("",include('customer.urls')),
+    path("",include('master.urls')),
+
+]
+
+urlpatterns+=[
+    re_path(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+]
