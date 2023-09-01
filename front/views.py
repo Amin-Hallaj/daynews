5cc6f858f55049e9c45601ad715e38179aad3758
Amin-Hallaj <aminhallajm@gmail.com>
2023-09-01 18:21:09 +0330

    ایجاد فانگش صفحه خانه

--- front/views.py
+++ front/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect , get_object_or_404
 
-# Create your views here.
+
+def front_index(request):
+    return render(request,'front/index.html',{})
