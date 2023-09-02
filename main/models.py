17fdcd23f952b50f02d8e7f80cd619004962a503
Amin-Hallaj <aminhallajm@gmail.com>
2023-09-02 22:52:38 +0330

    ساخت مدل در آپ مین

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
+class Main(models.Model):
+    name_site=models.CharField(max_length=100)
+    name_page=models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name_site} | {self.pk}"
