3a002eeba2c490ddf9f1499cb218242147238057
Amin-Hallaj <aminhallajm@gmail.com>
2023-09-19 21:48:10 +0330

    درست کردن شبکه های اجتماعی در مدل مین

--- main/models.py
+++ main/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class Main(models.Model):
     name_site=models.CharField(max_length=100)
     name_page=models.CharField(max_length=100)
+    about_us=models.TextField(null=True, blank=True)
+    instagram=models.CharField(max_length=100)
+    telegram=models.CharField(max_length=100)
+    whatsapp=models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.name_site} | {self.pk}"
