cae1de3787b43cbc79d71d512d2d5171dfeaa328
Amin-Hallaj <aminhallajm@gmail.com>
2023-09-20 00:07:09 +0330

    درست کردن فانگشن و یو ار ال برای آپ فرانت

--- front/urls.py
+++ front/urls.py
@@ -5,4 +5,13 @@ urlpatterns = [
     path('',views.front_index,name='front_index'),
     path('about/us/',views.front_about,name='front_about'),
     path('contact/us/',views.front_contact,name='front_contact'),
+    path('author/',views.front_author,name='front_author'),
+    path('categories/',views.front_categories,name='front_categories'),
+    path('post/list/',views.front_post_list,name='front_post_list'),
+    path('post/single/',views.front_post_single,name='front_post_single'),
+    path('search/result/',views.front_search_result,name='front_search_result'),
+    path('signin/',views.front_signin,name='front_signin'),
+    path('signup/',views.front_signup,name='front_signup'),
+    path('tag/',views.front_tag,name='front_tag'),
+    path('404/',views.front_404,name='front_404'),
 ]
--- front/views.py
+++ front/views.py
@@ -11,3 +11,39 @@ def front_about(request):
 
 def front_contact(request):
     return render(request,'front/contact-us.html',{})
+
+
+def front_author(request):
+    return render(request,'front/author.html',{})
+
+
+def front_categories(request):
+    return render(request,'front/categories.html',{})
+
+
+def front_post_list(request):
+    return render(request,'front/post-list.html',{})
+
+
+def front_post_single(request):
+    return render(request,'front/post-single.html',{})
+
+
+def front_search_result(request):
+    return render(request,'front/search-result.html',{})
+
+
+def front_signin(request):
+    return render(request,'front/signin.html',{})
+
+
+def front_signup(request):
+    return render(request,'front/signup.html',{})
+
+
+def front_tag(request):
+    return render(request,'front/tag.html',{})
+
+
+def front_404(request):
+    return render(request,'front/404.html',{})
