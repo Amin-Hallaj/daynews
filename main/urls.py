5685040a49a2ae5fec76b084a27ade1c19df0f62
Amin-Hallaj <aminhallajm@gmail.com>
2023-09-19 23:12:15 +0330

    فانگشن ورود و یو ار ال ورود

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('master/settings/site/',views.master_settings,name="master_settings"),
+    path('master/login/',views.master_login,name="master_login"),
+    path('master/login/submit/',views.master_login_submit,name="master_login_submit"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render , redirect , get_object_or_404
 
 def master_settings(request):
     return render(request,'master/settings.html',{})
+
+
+def master_login(request):
+    return render(request,'master/settings.html',{})
+
+
+def master_settings(request):
+    return render(request,'master/settings.html',{})
