1ec006f29ae30e796ab4faa712a8c7fcc5be8431
Amin-Hallaj <aminhallajm@gmail.com>
2023-09-22 21:58:43 +0330

    ایجاد بخش مدیریت سایت

--- master/urls.py
+++ master/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('dashboard/',views.master_dashboard,name='master_dashboard'),
+    path('dashboard/post/list',views.master_dashboard_post_list,name='master_dashboard_post_list'),
+    path('dashboard/post/categories/',views.master_dashboard_post_categories,name='master_dashboard_post_categories'),
+    path('dashboard/post/create/',views.master_dashboard_post_create,name='master_dashboard_post_create'),
+    path('dashboard/post/edit/',views.master_dashboard_post_edit,name='master_dashboard_post_edit'),
+    path('dashboard/author/list/',views.master_dashboard_author_list,name='master_dashboard_author_list'),
+    path('dashboard/author/single/',views.master_dashboard_author_single,name='master_dashboard_author_single'),
+    path('dashboard/edit/profile/',views.master_dashboard_edit_profile,name='master_dashboard_edit_profile'),
+    path('dashboard/reviews/',views.master_dashboard_reviews,name='master_dashboard_reviews'),
+    path('dashboard/settings/',views.master_dashboard_settings,name='master_dashboard_settings'),
 ]
--- master/views.py
+++ master/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect , get_object_or_404
 
-# Create your views here.
+
+def master_dashboard(request):
+    return render(request,'master/dashboard.html',{})
+
+
+def master_dashboard_post_list(request):
+    return render(request,'master/dashboard-post-list.html',{})
+
+
+def master_dashboard_post_categories(request):
+    return render(request,'master/dashboard-post-categories.html',{})
+
+
+def master_dashboard_edit_profile(request):
+    return render(request,'master/dashboard-edit-profile.html',{})
+
+
+def master_dashboard_post_create(request):
+    return render(request,'master/dashboard-post-create.html',{})
+
+
+def master_dashboard_author_list(request):
+    return render(request,'master/dashboard-author-list.html',{})
+
+
+def master_dashboard_author_single(request):
+    return render(request,'master/dashboard-author-single.html',{})
+
+
+def master_dashboard_post_edit(request):
+    return render(request,'master/dashboard-post-edit.html',{})
+
+
+def master_dashboard_reviews(request):
+    return render(request,'master/dashboard-reviews.html',{})
+
+
+def master_dashboard_settings(request):
+    return render(request,'master/dashboard-settings.html',{})
